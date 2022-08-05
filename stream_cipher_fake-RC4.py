8c350702f68a3d6e0973b08aebae0a75b611c9f4
djubran <57849683+djubran@users.noreply.github.com>
2022-08-05 12:26:39 +0300

    Update stream_cipher_fake-RC4.py

--- stream_cipher_fake-RC4.py
+++ stream_cipher_fake-RC4.py
@@ -16,57 +16,29 @@ Iterate over the plaintext
 XOR every character with the next byte of the pseudo-random generator's keystream
 Return the result as the ciphertext!
 
-
+p.s. passed all tests
 """
 
 
 
 def get_prg(plaintext_size, k):
-        i =j = 0  
-        keystream=[]
+        i=0 
+        j = 0  
+        keystream=""
         k_list=list(k)
-        print(len(k),plaintext_size)
-        length=min(len(k),plaintext_size)
-        for count in range(length):
+        for count in range(plaintext_size):
              i = (i + 1) % 32
              j = (j + ord(k_list[i]))%32
              k_list[i], k_list[j]= k_list[j],k_list[i]   # swap
-             Keystream_byte =str( k_list[((ord(k_list[i]) + ord(k_list[j])) %32)])
-             keystream.append(Keystream_byte)
-        keystream_str="".join(keystream)
-        return keystream_str
-"""
+             keystream += k_list[((ord(k_list[i]) + ord(k_list[j])) %32)]
+        return keystream
+    
+        
 def fake_rc4(plaintext, keystream):
-        plaintext_size=len(plaintext) 
-        plaintext_list=list(plaintext)
-        cipheredtext=[]
-        bytes1=[]
-        for count in range(len(plaintext_list)):
-            print('len(plaintext_list)',plaintext_size)
-            rand_key_byte=get_prg(plaintext_size, keystream,time)
-            print(rand_key_byte,type(rand_key_byte))
-            bytes1.append(rand_key_byte)
-            if len(keystream)<=1:
-                print('found it')
-                break
-        print('here',bytes1)
-        return encrypt(plaintext,bytes1)
-"""
-def fake_rc4(plaintext, keystream):
-    print('their shit',len(keystream))
-    keystream_rc4=get_prg(len(plaintext),keystream)
-    return encrypt(plaintext,keystream_rc4)
+    return encrypt(plaintext,keystream)
+
 
 def encrypt(plaintext, k):
     ciphertxt=[chr(ord(x)^ord(y)) for x,y in zip(plaintext,k)]
     final_ciphertxt="".join((ciphertxt ))
-    print('mine',final_ciphertxt)
-    return (final_ciphertxt)
-k=len('this is a text')
-fake_rc4('12345678901234567890123456789012345678901234567890','12345678901234567890123456789012')
-
-
-size=50 
-plaintxt='12345678901234567890123456789012345678901234567890'
-key ='12345678901234567890123456789012'
-#20113369639090048556658369838361121510153189493491
+    return final_ciphertxt
