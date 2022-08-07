fe25df8a86073dad67a64f225ceb2f20fbc24e7d
djubran <57849683+djubran@users.noreply.github.com>
2022-08-07 15:57:08 +0300

    Update hw2-ceaser_encryption.py

--- hw2-ceaser_encryption.py
+++ hw2-ceaser_encryption.py
@@ -1,3 +1,10 @@
+"""
+hw2- Implement Caesar’s cipher: implement a function encrypt that given a plaintext string and a key k (how many letters to shift), 
+returns a ciphertext where each character is shifted k places.
+You can assume all characters are lowercase letters, with no punctuation or spaces backward (so with k=2 'c' is encrypted by 'a')
+"""
+
+
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 def encrypt(plaintext, k):
