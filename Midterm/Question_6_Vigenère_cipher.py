429ae42f8eb5f405f69ec8be9b9552db0cae03ed
djubran <57849683+djubran@users.noreply.github.com>
2022-09-08 21:42:08 +0300

    Update Question_6_Vigenère_cipher.py

--- Midterm/Question_6_Vigenère_cipher.py
+++ Midterm/Question_6_Vigenère_cipher.py
@@ -48,3 +48,43 @@ def vigenere_encrypt(plaintext, codeword):
     return vigenere_enc_txt
     
 vigenere_encrypt('ATTACKATDAWN','LEMON')
+
+
+
+
+
+"""
+# plaintext and codeword in CAPITAL LETTERS  only
+
+alphabet_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def ceaser_encrypt(plaintext, k):
+    encryptedtext = []
+    alphabet = alphabet_lowercase.upper()
+    for letter in plaintext:
+        i = alphabet.index(letter)
+        j = (i + k) % len(alphabet)
+        encryptedtext.append(alphabet[j])
+    return ''.join(encryptedtext)
+
+    
+def vigenere_encrypt(plaintext, codeword):
+    vigenere_enc_txt = ''
+    if len(plaintext) == len(codeword): 
+       # codeword = list(codeword) 
+        pass
+    else: 
+        codeword = list(codeword) 
+        for i in range(len(plaintext) -len(codeword)-1): 
+          codeword.append(codeword[i % len(codeword)])
+          print('codeword repharsed is ',codeword)
+    for elem in range(0, len(codeword)):
+        print('A->' ,codeword[elem],' ',ord(codeword[elem])-ord('A'), 'convert this ',plaintext[elem] , 'ietration no. ',elem)
+        vigenere_enc_txt += ceaser_encrypt(plaintext[elem] ,ord(codeword[elem])-ord('A'))
+        print(vigenere_enc_txt)
+    print(len(plaintext),len(codeword),len(vigenere_enc_txt))
+    return vigenere_enc_txt
+    
+vigenere_encrypt('ASDFGHJK ','QWERTYUI')
+"""
