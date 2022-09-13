9b5ebfb625390b310375a0d7ac7af27b660e7517
djubran <57849683+djubran@users.noreply.github.com>
2022-09-13 18:40:11 +0300

    Create Hackxercise_2_static_signature.py

---
+++ Lesson 9 - Computer Viruses and How to Beat Them/Hackxercise_2_static_signature.py
@@ -0,0 +1,31 @@
+"""
+Implement the function sign so that given a line (in bytes), returns a unique signature of that line that is 20 characters long;
+Implement the scan function that, given a list of paths and a signature, reads them line by line, 
+and returns a list of all the paths that have a line that matches the static signature.
+Any line in a file might end with a ‘\n’. Remember to remove it.
+
+Don’t leave open files.
+
+SHA1 is a good way to get a digest of a string (Remember hashlib?).
+
+
+
+"""
+from hashlib import sha1
+
+def sign(line):
+    sha1_inst = sha1()
+    sha1_inst.update(line)
+    uniq_sign = sha1_inst.hexdigest()
+    return uniq_sign[0:20]
+
+def scan(paths, signature):
+    malicous_paths = []
+    for path in paths:
+        lines = open(path,'r').readlines()
+        for line in lines:        
+            line = line.rstrip('\n').encode('utf-8')
+            if signature == sign(line): #suspicous agent found
+                malicous_paths.append(path)
+                break
+    return malicous_paths
