451878b39f395d84b6f7d47dc5768ba320cdf3d9
djubran <57849683+djubran@users.noreply.github.com>
2022-09-13 20:20:33 +0300

    Update Hackxercise_4.1.2_backdoor_compiler's_compiler.py

--- Lesson 9 - Computer Viruses and How to Beat Them/Hackxercise_4.1.2_backdoor_compiler's_compiler.py
+++ Lesson 9 - Computer Viruses and How to Beat Them/Hackxercise_4.1.2_backdoor_compiler's_compiler.py
@@ -1,4 +1,21 @@
+"""
 
+The following format defines a basic authentication scheme:
+
+USERNAME: alice
+PASSWORD: 5!fxoP3
+These credentials are not known to the hacker, so they'd like to add a backdoor to this program, so that the 
+username hacker and the password 1234 work. However, they wouldn't want to change the original file, so they're thinking of changing its "compiler", 
+which takes this format and generates the following code:
+
+def authenticate(username, password):
+    return username == 'alice' and password == '5!fxoP3'
+
+Can you add a backdoor in the "compiler" to do so?
+
+here we'll do it by replacing the code in the complier's compiler the long way without global variables
+
+"""
 import re
 
 CODE = '''
