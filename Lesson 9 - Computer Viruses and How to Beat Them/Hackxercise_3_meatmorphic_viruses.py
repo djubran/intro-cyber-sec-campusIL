191aafbe60d59751f9244e0abceaf8efd5b9a2a4
djubran <57849683+djubran@users.noreply.github.com>
2022-09-13 19:17:14 +0300

    Create Hackxercise_3_meatmorphic_viruses.py

---
+++ Lesson 9 - Computer Viruses and How to Beat Them/Hackxercise_3_meatmorphic_viruses.py
@@ -0,0 +1,28 @@
+"""
+Write code that metamorphs the following expressions:
+
+x = x · 2
+s = 0
+for i in range(n):
+    s += i
+
+
+"""
+
+# x = x * 2
+# return x
+
+def first(x):
+    return x*2
+
+# s = 0
+# for i in range(n):
+#     s += i
+# return s
+
+def second(n):
+     s = 0
+     for i in range(n):
+         s += i
+     return s
+
